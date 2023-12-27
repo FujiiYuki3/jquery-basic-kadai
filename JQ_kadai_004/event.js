52875b814363f463061a6c03bf64ed8ffeaee19f
FujiiYuki3 <151428461+FujiiYuki3@users.noreply.github.com>
2023-12-27 23:36:17 +0900

    kadai_004をプッシュ

---
+++ JQ_kadai_004/event.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(document).on('load', console.log('loadイベントが発生しました'));
+});
+
+$(function(){
+    $(window).on('scroll', () => {
+            console.log('scrollイベントが発生しました');
+        });
+});
